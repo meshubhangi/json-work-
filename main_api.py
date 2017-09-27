ba3e247244b9030d4cd73f56d3ba1a53480e0fa1
Shubhangi Pandey <shubhangi.pandey08@gmail.com>
2017-09-27 18:13:40 +0530

    Update main_api.py

--- main_api.py
+++ main_api.py
@@ -10,21 +10,6 @@ url = main_api + urllib.parse.urlencode({'amount': amount})
 print(url)
 json_data = requests.get(url).json()
 print(json_data)
-
-#New codes with changes
-
-import urllib.parse
-import requests
-
-main_api ='https://opentdb.com/api.php?'
-amount = '10'
-category= '15'
-difficulty= 'easy'
-types= 'multiple'
-url = main_api + urllib.parse.urlencode({'amount': amount})
-print(url)
-json_data = requests.get(url).json()
-#print(json_data)
 json_response= json_data['response_code']
 print('response code:',json_response)
 print("enter the index")
